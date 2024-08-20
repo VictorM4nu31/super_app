a97a6e0cdd553dc53cceea5d3dfec01166b71f11
VictorM4nu31 <angelesvictor690@gmail.com>
2024-08-20 11:22:09 -0600

    Cambios realizados por erica

--- lib/screens/product/edit_product_screen.dart
+++ lib/screens/product/edit_product_screen.dart
@@ -39,7 +39,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
       quantity: int.parse(_quantityController.text),
       expirationDate: _expirationDate!,
       unitPrice: double.parse(_unitPriceController.text),
-      userId: widget.product.userId, // Incluye userId
+      userId: widget.product.userId,
     );
 
     _productService.updateProduct(updatedProduct).then((_) {
@@ -51,63 +51,105 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editar Producto'),
-        backgroundColor: Colors.blueGrey, // Color blueGrey para que se destaque
-        elevation: 4.0, // Elevación para que se vea elevada
+        backgroundColor: Colors.blueGrey,
+        elevation: 4.0,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Nombre'),
-            ),
-            TextField(
-              controller: _brandController,
-              decoration: const InputDecoration(labelText: 'Marca'),
-            ),
-            TextField(
-              controller: _quantityController,
-              decoration: const InputDecoration(labelText: 'Cantidad'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _unitPriceController,
-              decoration: const InputDecoration(labelText: 'Precio unitario'),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final selectedDate = await showDatePicker(
-                  context: context,
-                  initialDate: _expirationDate ?? DateTime.now(),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2100),
-                );
-                if (selectedDate != null) {
-                  setState(() {
-                    _expirationDate = selectedDate;
-                  });
-                }
-              },
-              child: Text(_expirationDate == null
-                  ? 'Seleccionar Fecha de Vencimiento'
-                  : 'Fecha de Vencimiento: ${_expirationDate.toString()}'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _editProduct,
-              child: const Text('Guardar Cambios'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _brandController,
+                decoration: InputDecoration(
+                  labelText: 'Marca',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _quantityController,
+                decoration: InputDecoration(
+                  labelText: 'Cantidad',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _unitPriceController,
+                decoration: InputDecoration(
+                  labelText: 'Precio unitario',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  final selectedDate = await showDatePicker(
+                    context: context,
+                    initialDate: _expirationDate ?? DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2100),
+                  );
+                  if (selectedDate != null) {
+                    setState(() {
+                      _expirationDate = selectedDate;
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellowAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Text(
+                  _expirationDate == null
+                      ? 'Seleccionar Fecha de Vencimiento'
+                      : 'Fecha de Vencimiento: ${_expirationDate.toString().split(' ')[0]}',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: _editProduct,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                icon: const Icon(Icons.save, color: Colors.white),
+                label: const Text(
+                  'Guardar Cambios',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
